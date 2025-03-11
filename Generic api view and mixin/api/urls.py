cc18d35ad1012df0163cf6bc67c1fcdd47113ea4
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2025-03-11 10:38:41 +0530

    task comleted

--- Generic api view and mixin/api/urls.py
+++ Generic api view and mixin/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
+from .views import GenericApiview, Createapi, Retrivemodelmixin, Updatedata, Deletedata
 urlpatterns = [
-    
+    path('', GenericApiview.as_view(), name="Generic api view" ),
+    path('create', Createapi.as_view(), name="Generic api create" ),
+    path('retrive/<int:pk>', Retrivemodelmixin.as_view(), name="Generic api retirve" ),
+    path('update/<int:pk>', Updatedata.as_view(), name="Generic api update" ),
+    path('delete/<int:pk>', Deletedata.as_view(), name="Generic api delete" ),
 ]
--- Generic api view and mixin/api/views.py
+++ Generic api view and mixin/api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
 
 # Create your views here.
+class GenericApiview(GenericAPIView, ListModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kawrgs):
+        return self.list(request, *args, **kawrgs)
+    
+
+class Createapi(GenericAPIView, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def post(self, request, *args, **kawrgs):
+        return self.create(request, *args, **kawrgs)
+    
+class Retrivemodelmixin(GenericAPIView, RetrieveModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kawrgs):
+        return self.retrieve(request, *args, **kawrgs)
+    
+class Updatedata(GenericAPIView, UpdateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kawrgs):
+        return self.update(request, *args, **kawrgs)
+    
+class Deletedata(GenericAPIView, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kawrgs):
+        return self.destroy(request, *args, **kawrgs)
