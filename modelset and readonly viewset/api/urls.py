0e8b5188f4bd46a251742105bbb01508c5d5ab09
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2025-03-12 12:39:24 +0530

    project in half way.

---
+++ modelset and readonly viewset/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import Stud
+# from rest_framework.routers import DefaultRouter
+
+
+urlpatterns = [
+    # path("", Stud.as_view(), name = "as Stud") 
+]
---
+++ modelset and readonly viewset/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .serializers import StudentSerializer
+from .models import Student
+
+# Create your views here.
+
+class Stud(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    
