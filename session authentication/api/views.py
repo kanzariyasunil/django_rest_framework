5024ff85c3720eacfa945a6688c05224af58f0f4
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2025-03-24 12:41:13 +0530

    session authentication in half way.

---
+++ session authentication/api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+# Create your views here.
+
+class Stud(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    # permission_classes = [IsAuthenticated]
+    # permission_classes = [AllowAny]
+    permission_classes = [IsAdminUser]
