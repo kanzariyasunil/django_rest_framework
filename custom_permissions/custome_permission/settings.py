f7f3ccc5c1045926b289e39804464af8adb671cc
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2025-03-27 15:41:39 +0530

    mysql insilize

--- custom_permissions/custome_permission/settings.py
+++ custom_permissions/custome_permission/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'custome_permission.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'serializer',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':3306
     }
 }
 
